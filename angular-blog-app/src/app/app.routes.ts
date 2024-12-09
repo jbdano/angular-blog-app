f1ee1a4748b02fa5108247127cb9b668c1b16ff2
Julian Dano <julian@told.design>
2024-12-08 23:05:19 -0500

    continued work on BlogPostComponent and moved directory

--- angular-blog-app/src/app/app.routes.ts
+++ angular-blog-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/blog', pathMatch: 'full' },
+    { path: 'blog', component: BlogComponent }
+];
