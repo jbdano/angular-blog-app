37921385c1ad1493f51c0df0401f596132ae4103
Julian Dano <julian@told.design>
2024-12-09 00:17:39 -0500

    removed unused components

--- angular-blog-app/src/app/app.component.ts
+++ angular-blog-app/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { HeaderComponent } from './global/header/header.component';
-import { FooterComponent } from './global/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet, RouterModule],
+  imports: [HeaderComponent, RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
